ee95ca0acb7738ec60c39de5c9733ad48f1c1ec8
dhanuarrasyid <rasyidpolinema@gmail.com>
2021-10-25 21:51:48 +0700

    Initial commit

---
+++ helpers/Menu.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-home "></i>'
+		),
+		
+		array(
+			'path' => 'pengguna', 
+			'label' => 'Pengguna', 
+			'icon' => '<i class="fa fa-user "></i>'
+		),
+		
+		array(
+			'path' => 'surat_masuk', 
+			'label' => 'Surat Masuk', 
+			'icon' => '<i class="fa fa-inbox "></i>'
+		)
+		
+		
+	);
+		
+	
+	
+}
