6f3c8c3c3f2650de1a1e049c97ee7b01cde93837
Firandhanu A. R <60869225+dhanuarrasyid@users.noreply.github.com>
2021-10-26 07:06:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -87,7 +87,7 @@ define("DEFAULT_EMAIL_ACCOUNT_NAME", "");
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
-define("DB_NAME", "aplikasisurat2021");
+define("DB_NAME", "aplikasisurat");
 define("DB_TYPE", "mysql");
 define("DB_PORT", "");
 define("DB_CHARSET", "utf8");
